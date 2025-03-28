fec1403abe3e605d11c1beef79dccbf15fa1e449
donotsaylazy <769031940@qq.com>
2025-03-28 09:46:28 +0800

    初始化

---
+++ demo-design-1-00/src/main/java/demo/design/card/IQiYiCard.java
@@ -0,0 +1,13 @@
+package demo.design.card;
+
+/**
+ * @PackageName: demo.design.card
+ * @ClaseName: IQiYiCard
+ * @Description: IQiYiCard
+ * @Author: zhaoyg
+ * @Date: 2025/3/28 9:45
+ * @params *@params $
+ */
+
+public class IQiYiCard {
+}
